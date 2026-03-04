5d684f41aa448d742245fa71e598eb63cf199f09
Ryan C <rjchou@icloud.com>
2026-03-04 08:28:34 -0800

    macos compat

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
@@ -22,27 +21,37 @@ struct State {
 impl State {
     async fn new(window: Arc<Window>) -> State {
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
-        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions::default()).await.unwrap();
-        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor::default()).await.unwrap();
-        
-        
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions::default())
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await
+            .unwrap();
+
         let size = window.inner_size();
         let surface = instance.create_surface(window.clone()).unwrap();
         let cap = surface.get_capabilities(&adapter);
         let surface_format = cap.formats[0];
-        
+
         let state = State {
-            window, device, queue, size, surface, surface_format,
+            window,
+            device,
+            queue,
+            size,
+            surface,
+            surface_format,
         };
-        
+
         state.configure_surface();
         state
     }
-    
+
     fn get_window(&self) -> &Window {
         &self.window
     }
-    
+
     fn configure_surface(&self) {
         let surface_config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
@@ -52,24 +61,29 @@ impl State {
             width: self.size.width,
             height: self.size.height,
             desired_maximum_frame_latency: 2,
-            present_mode: wgpu::PresentMode::Mailbox,
+            // present_mode: wgpu::PresentMode::Mailbox,
+            present_mode: wgpu::PresentMode::AutoVsync,
         };
         self.surface.configure(&self.device, &surface_config);
     }
-    
+
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         self.size = new_size;
         self.configure_surface();
     }
-    
+
     fn render(&mut self) {
-        let surface_texture = self.surface.get_current_texture().expect("Error: failed to acquire the next swapchain texture");
-        let texture_view = surface_texture.texture.create_view(&wgpu::TextureViewDescriptor {
-            format: Some(self.surface_format.add_srgb_suffix()),
-            ..Default::default()
-        });
-        
-        
+        let surface_texture = self
+            .surface
+            .get_current_texture()
+            .expect("Error: failed to acquire the next swapchain texture");
+        let texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor {
+                format: Some(self.surface_format.add_srgb_suffix()),
+                ..Default::default()
+            });
+
         let mut encoder = self.device.create_command_encoder(&Default::default());
         let renderpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: None,
@@ -87,14 +101,13 @@ impl State {
             occlusion_query_set: None,
             multiview_mask: None,
         });
-        
+
         drop(renderpass);
         self.queue.submit([encoder.finish()]);
         self.window.pre_present_notify();
         surface_texture.present();
     }
 }
-
 
 #[derive(Default)]
 struct App {
@@ -103,21 +116,25 @@ struct App {
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = Arc::new(event_loop.create_window(Window::default_attributes()).unwrap());
-        
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
         let state = pollster::block_on(State::new(window.clone()));
         self.state = Some(state);
-        
+
         window.request_redraw();
     }
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         let state = self.state.as_mut().unwrap();
-        
+
         match event {
             WindowEvent::CloseRequested => {
                 println!("Close requested! Exiting application...");
                 event_loop.exit();
-            },
+            }
             WindowEvent::RedrawRequested => {
                 state.render();
                 state.get_window().request_redraw();
@@ -131,10 +148,10 @@ impl ApplicationHandler for App {
     }
 }
 
-fn main() { 
+fn main() {
     let events = EventLoop::new().unwrap();
     events.set_control_flow(ControlFlow::Poll);
-    
+
     let mut app = App::default();
     match events.run_app(&mut app) {
         Ok(()) => println!("A-OK!"),
